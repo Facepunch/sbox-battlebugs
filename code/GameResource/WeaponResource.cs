48d36f695c26fe4ac573704405ac1900afb24d6b
Carson Kompon <carsokompo@gmail.com>
2024-07-02 10:39:30 -0400

    Added Cost to WeaponResource

--- code/GameResource/WeaponResource.cs
+++ code/GameResource/WeaponResource.cs
@@ -6,7 +6,9 @@ public class WeaponResource : GameResource
     public string Name { get; set; } = "Rock";
     [TextArea] public string Description { get; set; } = "";
     [ImageAssetPath] public string Icon { get; set; }
-    public int StartingAmount { get; set; } = 1;
+
+    [Group( "Stats" )] public int StartingAmount { get; set; } = 1;
+    [Group( "Stats" )] public int Cost { get; set; } = 20;
     [Group( "Attack" )] public RangedFloat AmountFired { get; set; } = new RangedFloat( 1 );
     [Group( "Attack" )] public RangedFloat Damage { get; set; } = new RangedFloat( 8 );
     [Group( "Attack" )] public float Spray { get; set; } = 1f;
